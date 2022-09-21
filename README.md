3f40433ebd86e7190f9f2a589b46efd88c638f0b
RealXun <112812176+RealXun@users.noreply.github.com>
2022-09-21 20:23:33 +0200

    upate

--- README.md
+++ README.md
@@ -18,3 +18,21 @@ https://es.wikipedia.org/wiki/Piedra,_papel_o_tijera
 6. Logical operators.
 7. Print by console
 8. Import of external modules
+
+## Suggested
+- Importa la función choice del módulo random
+- https://stackoverflow.com/questions/306400/how-to-randomly-select-an-item-from-a-list
+- Asigna a una lista las 3 posibles opciones: 'piedra', 'papel' o 'tijeras'. 
+- Asigna una variable al número de partidas máxima: 1, 3, 5, etc...
+- Asigna una variable al número de partidas que debe ganar un jugador para ganar. 
+- Preferiblemente el valor será en función de el número de partidas máximas
+- Define una función que devuelva aleatoriamente una de las 3 opciones. 
+- Esto corresponderá a la jugada de la máquina. Totalmente aleatoria. 
+- Define una función que pregunte tu elección: 'piedra', 'papel' o 'tijeras' sólo debe permitir una de las 3 opciones. Esto es programación defensiva. Si no es piedra, papel o tijeras sigue preguntando hasta que lo sea. 
+- Define una función que resuelva un combate. 
+- Devuelve 0 si hay empate, 1 si gana la máquina, 2 si gana el jugador humano 
+- Define una función que muestre la elección de cada jugador y el estado de la partida
+- Esta función debe utilizarse cada vez que se actualicen los puntos acumulados 
+- Crea dos variables que acumulen las partidas ganadas de cada participante
+- Crea un bucle que itere mientras ningún jugador alcance el mínimo de victorias necesarias para ganar. Dentro del bucle resuelve la jugada de la máquina y pregunta la del jugador. Comparalas y actualiza el valor de las variables que acumulen las partidas ganadas de cada participante.   
+- Anuncia por consola el ganador del juego en función de quién tiene más victorias aculumadas
---
+++ Test.py
@@ -0,0 +1,85 @@
+def is_odd(first_input):
+    if first_input % 2 != 0:
+        return True
+    else:
+        False
+
+def is_more_or_equal_3(first_input):
+    if first_input >= 3:
+        return True
+    else:
+        return False
+        
+while 1:
+    first_input = int(input("Maximum number of games? (minimum3): "))
+    if is_odd(first_input) and is_more_or_equal_3(first_input):
+        print("Input taken successfully.","Number is odd and greater than five.","The game starts now:")
+        partidasplayed=0
+        max_rounds=first_input
+        played=0
+        machine_win=0
+        users_win=0
+        to_win=int((max_rounds/2)+0.5)
+        games_left=max_rounds-1
+        while played<max_rounds:
+            import sys
+            import random
+            random = random.randrange(0, 3)
+            print("1)rock")
+            print("2)paper")
+            print("3)scissors")
+            if random == 0:
+                machine = "rock"
+            elif random == 1:
+                machine = "paper"
+            elif random == 2:
+                machine = "scissors"
+            option = int(input("What do you want to use?:"))
+            while option > 3 or option==0:
+                print("Incorrect option.")# pick again
+            if option == 1:
+                user = "rock"
+            elif option == 2:
+                user = "paper"
+            elif option == 3:
+                user = "scissors"
+            print("\nYou choose: ", user)
+            print("The machine has chosen: ", machine)
+            if machine == "rock" and user == "paper":
+                print("\nYou win!!, paper covers rock")
+                users_win+=1
+            elif machine == "paper" and user == "scissors":
+                print("\nYou win!!, scissors cuts paper")
+                users_win+=1
+            elif machine == "scissors" and user == "rock":
+                print("\nYou win!!, rock crushes scissors")
+                users_win+=1
+            elif machine == "paper" and user == "rock":
+                print("\nYou lost!!, paper covers rock")
+                machine_win+=1
+            elif machine == "scissors" and user == "paper":
+                print("\nYou lost!!, scissors corta paper")
+                machine_win+=1
+            elif machine == "rock" and user == "scissors":
+                print("\nYou lost!!, rock crushes scissors")
+                machine_win+=1
+            elif machine == user:
+                print("\nTIE!!!")   
+            print("Just",games_left,"game left to play")               
+            print("\nPlayer won",users_win ,"games")
+            print("The machine won",machine_win ,"games\n")
+            played+=1
+            games_left-=1
+            if users_win==to_win or machine_win==to_win:
+                    break
+        if users_win<machine_win or machine_win==to_win:
+            print("The machine is the winner!!!! Machine did win ",machine_win ,"games")
+        elif users_win==to_win or machine_win<users_win:
+            print("User is the winner!!!! User did win ",users_win ,"games")           
+        else:
+            print("There are no winners, TIE!!!!")
+        sys.exit() #lo he puesto para que el loop infinito termine al llegar al numero de partidas que se querian jugar
+    else:
+        if not is_odd(first_input) or is_more_or_equal_3(first_input):
+            print("Failed! The number of games must be odd and greater than or equal to 5")
+            continue
